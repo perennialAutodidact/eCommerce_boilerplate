31adf7d0a70e02ea9baa556927a8a025d80db400
dirtTastesGood <keegood8@gmail.com>
2020-08-14 17:03:03 -0700

    added authentication classes to settings.py

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -107,7 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+
 }
 
 
